801823a53b160ae4f9645cb6c1f628aacfbbac51
amyusup <amyusup26@gmail.com>
2020-12-22 16:50:38 +0700

    setup after build

---
+++ build/precache-manifest.dbb7c6d704affbc6271784f0c82938ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1266b0198c0a853f5b32cbc91700c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdd450b54c6ec155df55",
+    "url": "/static/css/2.dcd9fce7.chunk.css"
+  },
+  {
+    "revision": "ac8a65b184029962f98b",
+    "url": "/static/css/main.99dadb77.chunk.css"
+  },
+  {
+    "revision": "bdd450b54c6ec155df55",
+    "url": "/static/js/2.3d36aa79.chunk.js"
+  },
+  {
+    "revision": "8c7c0f9e3a023eb3b2d8a3552596d18d",
+    "url": "/static/js/2.3d36aa79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8a65b184029962f98b",
+    "url": "/static/js/main.2e4353a8.chunk.js"
+  },
+  {
+    "revision": "44b815fc4c8efdb61e32",
+    "url": "/static/js/runtime-main.2afdd928.js"
+  }
+]);
