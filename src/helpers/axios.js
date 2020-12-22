39bb8e6eaeb60021b944dd874bc7619fbda00069
amyusup <amyusup26@gmail.com>
2020-12-22 15:30:23 +0700

    update aws

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://18.212.1.32:4444/api/v1",
+  baseURL: "http://54.82.50.0:4444/api/v1",
   // baseURL: "http://localhost:4444/api/v1",
   // baseURL: "https://zwallet-team.herokuapp.com/api/v1",
 });
