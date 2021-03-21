b162ae1a984cc1af4f9900249b4533c31db9c5b6
amyusup <amyusup26@gmail.com>
2021-03-21 15:07:20 +0700

    update backend host

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://54.82.50.0:4444/api/v1",
+  // baseURL: "http://54.82.50.0:4444/api/v1",
   // baseURL: "http://localhost:4444/api/v1",
-  // baseURL: "https://zwallet-team.herokuapp.com/api/v1",
+  baseURL: "https://zwallet-team.herokuapp.com/api/v1",
 });
